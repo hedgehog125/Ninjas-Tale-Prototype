a042c4c052c96746d1560e9045587e03a296c224
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2021-11-03 18:04:23 +0000

    Video 1

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour {
-	public float moveSpeed;
+	[SerializeField] private float moveSpeed;
+	//[SerializeField] private float maxSpeed;
 
 	private Vector2 move;
 	private bool jump;
@@ -24,6 +25,25 @@ public class playerMovement : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		rb.AddForce(new Vector2(move.x * moveSpeed, jump? 5 : 0));
-    }
+		// This gives a less physics-y feel compared to addForce and means movements don't last as long
+
+		/*
+		Vector2 newVel = rb.velocity;
+		newVel.x += move.x * moveSpeed;
+		if (Mathf.Abs(newVel.x) > maxSpeed) newVel.x = maxSpeed * Mathf.Sign(newVel.x);
+
+		if (jump) {
+			newVel.y += 2;
+		}
+
+		rb.velocity = newVel;
+		*/
+		//rb.AddForce(new Vector2(move.x * moveSpeed, 0));
+
+		Vector2 newVel = new Vector2(move.x * moveSpeed, rb.velocity.y);
+		if (jump) {
+			newVel.y += 1;
+		}
+		rb.velocity = newVel;
+	}
 }
