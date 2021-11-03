e2d42fcb9e0786fbf4c3d509236db9e2f66c01f8
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2021-11-03 22:15:52 +0000

    Fixed the UI, improved the jumping and made the ninja sprite

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -8,20 +8,51 @@ public class playerMovement : MonoBehaviour {
 	//[SerializeField] private float maxSpeed;
 
 	private Vector2 move;
-	private bool jump;
+	private bool isJumping;
+
 	private Rigidbody2D rb;
+	private Collider2D col;
+	private SpriteRenderer ren;
 
-
-	void Awake() {
-		rb = GetComponent<Rigidbody2D>();
-	}
+	private List<GameObject> onground = new List<GameObject>();
 
 	void OnMove(InputValue movementValue) {
 		move = movementValue.Get<Vector2>();
 	}
-
 	void OnJump(InputValue input) {
-		jump = input.Get<float>() > 0;
+		isJumping = input.Get<float>() > 0;
+	}
+
+
+	void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+		col = GetComponent<Collider2D>();
+		ren = GetComponent<SpriteRenderer>();
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag("Standable")) {
+			float bottomY = transform.position.y - (col.bounds.size.y / 4);
+			bool below = false;
+			int count = collision.contactCount;
+			for (int i = 0; i < count; i++) {
+				Vector2 point = collision.GetContact(i).point;
+				if (point.y <= bottomY) { // Point of contact has to be in the bottom quarter.
+					below = true;
+					break;
+				}
+			}
+			if (below) {
+				onground.Add(collision.gameObject);
+			}
+		}
+
+	}
+	void OnCollisionExit2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag("Standable")) {
+			onground.Remove(collision.gameObject);
+		}
 	}
 
 	void FixedUpdate() {
@@ -41,9 +72,14 @@ public class playerMovement : MonoBehaviour {
 		//rb.AddForce(new Vector2(move.x * moveSpeed, 0));
 
 		Vector2 newVel = new Vector2(move.x * moveSpeed, rb.velocity.y);
-		if (jump) {
-			newVel.y += 1;
+		if (isJumping && onground.Count != 0) {
+			newVel.y += 5;
 		}
 		rb.velocity = newVel;
+
+		// Flipping left/right
+		if (Mathf.Abs(rb.velocity.x) > 0.1f) {
+			ren.flipX = rb.velocity.x < 0;
+		}
 	}
 }
