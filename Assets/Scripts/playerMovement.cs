4231e5517efeb9dbbb5248fd4377c2c5686780f3
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2021-11-03 00:53:01 +0000

    Downgraded the Unity version and added a light to the player. Also added flight

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,7 +7,10 @@ public class playerMovement : MonoBehaviour {
 	public float moveSpeed;
 
 	private Vector2 move;
+	private bool jump;
 	private Rigidbody2D rb;
+
+
 	void Awake() {
 		rb = GetComponent<Rigidbody2D>();
 	}
@@ -16,7 +19,11 @@ public class playerMovement : MonoBehaviour {
 		move = movementValue.Get<Vector2>();
 	}
 
+	void OnJump(InputValue input) {
+		jump = input.Get<float>() > 0;
+	}
+
 	void FixedUpdate() {
-		rb.AddForce(new Vector2(move.x * moveSpeed, 0));
+		rb.AddForce(new Vector2(move.x * moveSpeed, jump? 5 : 0));
     }
 }
