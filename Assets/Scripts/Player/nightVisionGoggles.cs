0fa298a430e401b8006f575f761dce0fc1aaef31
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2021-11-21 21:17:15 +0000

    Started trying to add different lighting depending on if you're in a light area or not

--- Assets/Scripts/Player/nightVisionGoggles.cs
+++ Assets/Scripts/Player/nightVisionGoggles.cs
@@ -2,33 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class nightVisionGoggles : MonoBehaviour {
     [Header("Game Objects")]
     [SerializeField] private GameObject goggles;
+	[SerializeField] private GameObject minLights;
 
-    // Also read by the controller script
-    [Header("When Active")]
+	// Also read by the controller script
+	[Header("When Active")]
     [SerializeField] public float lightGlobalIntensity;
     [SerializeField] public float lightMultiplier;
 
-    [Header("Colours When Active")]
+	[Header("When In Light Area")]
+	[SerializeField] public float lightAreaSkyReduce;
+	[SerializeField] public float lightAreaObjectReduce;
+	[SerializeField] public float lightAreaChangeSpeed;
+
+
+	[Header("Colours When Active")]
     [SerializeField] public Color lightGlobalColor;
     [SerializeField] public Color lightSkyColor;
 
     private bool gogglesInput;
 
     private nightVisionAnimation gogglesScript;
+	private enemyAlerter alertScript;
+	private Light2D minLightObjects;
+	private Light2D minLightSky;
 
-    [HideInInspector] public bool active;
+	[HideInInspector] public bool active; // Set by animation script
 
-    private void Awake() {
+	private float originalMinLightObjects;
+	private float originalMinLightSky;
+
+	private void Awake() {
         gogglesScript = goggles.GetComponent<nightVisionAnimation>();
+		alertScript = GetComponent<enemyAlerter>();
 
-        if (lightGlobalColor.a != 1) {
+		minLightObjects = minLights.GetComponent<Light2D>();
+		minLightSky = minLights.transform.GetChild(0).GetComponent<Light2D>();
+		originalMinLightObjects = minLightObjects.intensity;
+		originalMinLightSky = minLightSky.intensity;
+
+
+		if (lightGlobalColor.a < 0.99f) {
             Debug.LogWarning("Global light colour is at least partly transparent.");
         }
-        if (lightSkyColor.a != 1) {
+        if (lightSkyColor.a < 0.99f) {
             Debug.LogWarning("Sky light colour is at least partly transparent.");
         }
     }
@@ -37,7 +58,7 @@ public class nightVisionGoggles : MonoBehaviour {
         gogglesInput = input.isPressed;
     }
 
-    private void FixedUpdate() {
+    private void Update() {
         if (! gogglesScript.animating) {
             if (gogglesInput) {
                 active = ! active;
@@ -46,5 +67,17 @@ public class nightVisionGoggles : MonoBehaviour {
                 gogglesInput = false;
             }
         }
+
+		if (active) {
+			minLightObjects.intensity = originalMinLightObjects;
+			minLightSky.intensity = originalMinLightSky;
+		}
+		else {
+			if (alertScript.inLight) {
+				if ()
+				minLightObjects.intensity -= lightAreaChangeSpeed * Time.deltaTime;
+				minLightSky.intensity += ;
+			}
+		}
     }
 }
